9f08fba947d2e5f5319a5b0801db348787e2915d
Erick Garavito <sangaravi22@gmail.com>
2024-03-14 22:58:37 -0500

    Spring boot project

---
+++ Codigo/arquitectura/src/test/java/arquitectura/arquitectura/ArquitecturaApplicationTests.java
@@ -0,0 +1,13 @@
+package arquitectura.arquitectura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquitecturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
